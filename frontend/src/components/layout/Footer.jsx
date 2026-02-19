3fef83d170dc5ca22acc79a3ec08d2eddc8ad5f6
Athov07 <atharvadsare17@gmail.com>
2026-02-19 15:46:34 +0530

    Footer changes

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bg-white shadow-inner mt-10">
-      <div className="max-w-7xl mx-auto px-4 py-6 text-center text-gray-500 text-sm">
+    <footer className="bg-white shadow-[0_-4px_10px_rgba(0,0,0,0.20)] mt-10">
+      <div className="max-w-7xl mx-auto px-4 py-4 text-center text-gray-500 text-sm">
         {new Date().getFullYear()} MyApp. All rights reserved.
       </div>
     </footer>
